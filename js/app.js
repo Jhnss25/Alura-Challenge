6422579ae85b22cf03dafd2dbe4aa31703669303
Jhnss25 <jhnsaave1997@gmail.com>
2023-05-17 06:58:46 -0500

    encriptador en app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,47 @@
 
 
 function encriptador(texto) {
+    texto = texto.toLowerCase().split("");
+
+    for (let i = 0; i < texto.length; i++) {
+        switch (texto[i]) {
+            case 'a':
+                texto[i] = 'ai';
+                break;
+            case 'e':
+                texto[i] = 'enter';
+                break;
+            case 'i':
+                texto[i] = 'imes';
+                break;
+            case 'o':
+                texto[i] = 'ober';
+                break;
+            case 'u':
+                texto[i] = 'ufat';
+                break;
+        }
+    }
     
+
+    return texto.join("");
 }
 
-encriptador('Esto es un encriptador, hola como estas');
+console.log(encriptador('Esto es un encriptador, hola como estas'));
+console.log(encriptador('aeiouuoiea'));
+console.log(encriptador('ai'));
+
+function desencriptar(encriptado) {
+    let descriptado = encriptado.toLowerCase()
+        .replace(/ai/g, 'a')
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+
+    return descriptado;
+}
+
+console.log(desencriptar(encriptador('Esto es un encriptador, hola como estas')));
+console.log(desencriptar(encriptador('aeiouuoiea')));
+console.log(desencriptar(encriptador('ai')));
